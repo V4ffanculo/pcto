b38cdcec0615241335371dde1276159ce19eda52
V4ffanculo <127745427+V4ffanculo@users.noreply.github.com>
2023-03-13 15:46:05 +0100

    Add files via upload

---
+++ esempio.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main ()
+{
+    int numero = 42;
+    double numerolungo = 54475477;
+    float numeroconlavirgola = 5.676;
+    char carattere = 'c';
+    char c = 99;
+
+    printf("il numero é: %d\n", numero);
+    printf("numero lungo é: %f, numero con la virgopla é: %f\n", numerolungo, numeroconlavirgola);
+    printf("il carattere é %c\n", carattere);
+}
