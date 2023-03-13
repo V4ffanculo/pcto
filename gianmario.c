948a30a238522a64ede8893518adc2e80245e677
V4ffanculo <127745427+V4ffanculo@users.noreply.github.com>
2023-03-13 16:47:24 +0100

    Add files via upload

---
+++ gianmario.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+int x;
+int y;
+
+printf("inserisci un nurero: ");
+scanf("%d", &x);
+printf("inserisci un altro nurero: ");
+scanf("%d", &y);
+printf("i numeri inseriti sono x = %d e y = %d\n", x, y);
+}
