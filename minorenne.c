82e995c55453932da63528f70c77dca7ae06e5a7
V4ffanculo <127745427+V4ffanculo@users.noreply.github.com>
2023-03-14 15:18:03 +0100

    Add files via upload

--- minorenne.c
+++ minorenne.c
@@ -3,16 +3,37 @@
 int main ()
 
 {
-    int eta;
-    printf("quanti anni hai ?");
-    scanf("%d", &eta);
+    int x;
+    int y;
+    int z;
 
-    if (eta < 18)
+    printf("é il lato di un triangolo?");
+    scanf("%d", &x);
+    printf("é il lato di un triangolo?");
+    scanf("%d", &y);
+    printf("é il lato di un triangolo?");
+    scanf("%d", &z);
+    if ((x+y>z)&&(x+z>y)&&(y+z>x))
     {
-        printf("minorenne\n");
+        printf("sono lati di un triangolo\n");
+        if (x==y && y==z)
+        {
+            printf("é equilatero\n");
+        }
+        else if (x!=y && y!=z)
+        {
+            printf("é scaleno\n");
+        }
+        else 
+        {
+            printf("é isoscele\n");
+        }
     }
-    else
-    {
-        printf("maggiorenne\n");
-    }
+
+else 
+{
+    printf("non é un triangolo\n");
+}       
 }
+
+
